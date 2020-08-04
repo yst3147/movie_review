863a65b3729cbd660a4290a67c73ef4c3178e564
styoo <yst3147@naver.com>
2020-08-04 12:16:47 +0900

    movie_data.csv

--- movie_data.py
+++ movie_data.py
@@ -10,11 +10,21 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 movie = soup.select('.lst_detail_t1 > li')
 
-movie_data = {}
-for m in movie:
-    code = m.select_one('.lst_dsc > .tit > a')['href']
-    title = m.select_one('.lst_dsc > .tit > a').text
-    codeurl, code_num = code.split("=")
-    movie_data['title'] = title
-    movie_data['code'] = code_num
-    print(movie_data)
+
+with open('./movie_data.csv', 'w', encoding='utf-8-sig', newline='') as file:
+    fieldnames = ['title', 'code']
+    csvfile = csv.DictWriter(file, fieldnames = fieldnames)
+    csvfile.writeheader()
+
+    for m in movie:
+        movie_data = {}
+        code = m.select_one('.lst_dsc > .tit > a')['href']
+        title = m.select_one('.lst_dsc > .tit > a').text
+
+        codeurl, code_num = code.split("=")
+
+        movie_data['title'] = title
+        movie_data['code'] = code_num
+        print(movie_data)
+
+        csvfile.writerow(movie_data)
